c3d3ffb6baf80f5a3cf84b88a76d6e6468be34d6
julianboyne <julianboyne11@gmail.com>
2022-11-01 17:50:04 -0400

    largest num in list challenge

--- python_functions.py
+++ python_functions.py
@@ -1,5 +1,8 @@
 #Challenge 1
 
+from re import A
+
+
 def sum_to(num):
   sum = 0
   for n in range(1, num + 1):
@@ -8,4 +11,13 @@ def sum_to(num):
 
 print(sum_to(10))
 
+#Challenge 2
 
+def largest(list):
+  max = list[0]
+  for n in list:
+    if n > max:
+      max = n
+  return max
+
+print(largest([1, 2, 3, 4, 0]))
