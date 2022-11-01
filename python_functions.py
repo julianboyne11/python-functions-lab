34d9787529a9e3f3cfd9f76c63066811635ec250
julianboyne <julianboyne11@gmail.com>
2022-11-01 17:59:39 -0400

    challenge 3 ocurrences

--- python_functions.py
+++ python_functions.py
@@ -9,7 +9,7 @@ def sum_to(num):
     sum += n
   return sum
 
-print(sum_to(10))
+# print(sum_to(10))
 
 #Challenge 2
 
@@ -20,4 +20,16 @@ def largest(list):
       max = n
   return max
 
-print(largest([1, 2, 3, 4, 0]))
+# print(largest([1, 2, 3, 4, 0]))
+
+#Challenge 3
+
+def occurrences(str1, substr2):
+  return str1.count(substr2)
+  # count = 0
+  # for char in str1:
+  #   if char == substr2:
+  #     count += 1
+  # return count
+
+print(occurrences('fleep floop', 'p'))
